588c1e8700abeb1e295c2fd1f4e349fb09531c57
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-03 02:34:28 -0300

    Iniciando validações

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -5,7 +5,12 @@ const router = express.Router();
 // Controller 
 const {register} = require("../controllers/UserController")
 
+//Middlewares
+const validate = require("../middlewares/handleValidation")
+
+
+
 // Routes
-router.post("/register", register);
+router.post("/register", validate, register);
 
 module.exports = router;
