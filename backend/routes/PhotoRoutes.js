0465ff8d0cd95335cebdbd872824ab36bafa5b4a
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-04 12:03:17 -0300

    Funcionalidade de busca de fotos

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto} = require("../controllers/PhotoController");
+const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto, searchPhotos} = require("../controllers/PhotoController");
 
 // Middlewares
 const {photoInsertValidation, photoUpdateValidation, commentValidation} = require("../middlewares/photoValidation");
@@ -21,6 +21,7 @@ insertPhoto
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/search", authGuard, searchPhotos);
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 router.put("/like/:id", authGuard, likePhoto);
