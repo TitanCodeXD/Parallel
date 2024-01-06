582508dffbce3ff9076ea41a8109a490fe78dc7b
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-06 11:34:34 -0300

    Criando component Navbar

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+// React
+import React from 'react'
+
+// CSS
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+    <footer id = "footer">
+        <p>Parallel &copy; 2024.</p>
+    </footer>
+  )
+}
+
+export default Footer
