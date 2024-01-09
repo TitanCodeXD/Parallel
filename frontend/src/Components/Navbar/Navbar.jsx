23b07ebc6d79b3f1780f4136ed9bd2338b9a6562
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-09 17:33:21 -0300

    Navbar para usuários autenticados

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,22 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
 import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from 'react-icons/bs';
 
+// Hooks
+import { useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 // CSS
 import './Navbar.css';
 
 // Logo
 import Logo from '../Logo/Logo';
 
+
 const Navbar = () => {
+  const {auth} = useAuth();
+  const {user} = useSelector((state) => state.auth);
 
   return (
     <nav id="nav">
@@ -19,19 +28,44 @@ const Navbar = () => {
             <input type = 'text' placeholder = 'Pesquisar'></input>
         </form>
         <ul id = "nav-links">
-            <li>
+            {auth ? (
+              <>
+              <li>
             <NavLink to = "">
                 <BsHouseDoorFill />
             </NavLink>
             </li>
+            {user && (
+              <li>
+                <NavLink to = {`/users/${user._id}`}>
+                  <BsFillCameraFill/>
+                </NavLink>
+              </li>
+            )}
 
             <li>
+              <NavLink to = "profile">
+              <BsFillPersonFill />
+              </NavLink>
+            </li>
+
+            <li>
+              <span>Sair</span>
+            </li>
+
+              </>
+            ) : (
+              <>
+              <li>
             <NavLink to = '/login'>Entrar</NavLink>
             </li>
 
             <li>
             <NavLink to = '/register'>Cadastrar</NavLink>
             </li>
+              </>
+            )}
+            
         </ul>
     </nav>
   )
