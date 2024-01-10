7034d3554c4cdab3d03757786d7e8472c8f85724
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-10 18:52:45 -0300

    Exibindo dados no perfil

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -15,7 +15,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // Redux
-
+import { getUserDetails } from '../../slices/userSlice';
 
 // CSS
 import './Profile.css';
@@ -23,10 +23,37 @@ import './Profile.css';
 
 const Profile = () => {
 
+  const {id} = useParams();
+
+  const dispatch = useDispatch();
+
+  const {user, loading} = useSelector((state) => state.user) 
+  const {user: userAuth} = useSelector((state) => state.auth)
+
+  // Photo
+
+
+  // Load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id])
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
 
   return (
-    <div>
-        Profile
+    <div id = "profile">
+        <div className = 'profile-header'>
+          {user.profileImage && (
+            <img src = {`${uploads}/users/${user.profileImage}`} alt = {user.name}/>
+          )}
+          <div className = 'profile-description'>
+            <h2>{user.name}</h2>
+            <p>{user.bio}</p>
+          </div>
+        </div>
     </div>
   )
 }
