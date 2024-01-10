baf7e9f30000a433f27a06ea841f101a25aabe9e
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-10 00:25:36 -0300

    Service para resgatar dados do usuário por id

--- frontend/src/services/userService.jsx
+++ frontend/src/services/userService.jsx
@@ -35,9 +35,30 @@ const updateProfile = async(data, token) => {
     }
 };
 
+// Get user details 2
+const getUserDetails = async (id) => {
+
+    const config = requestConfig("GET")
+
+    try {
+        
+        const res = await fetch(api + "/users/" + id, config)
+                    .then((res) => res.json())
+                    .catch((err) => err)
+
+        return res;
+    } catch (error) {
+        
+        console.log(error);
+
+    }
+};
+
+
 const userService = {
     profile,
     updateProfile,
+    getUserDetails,
 };
 
 export default userService;
