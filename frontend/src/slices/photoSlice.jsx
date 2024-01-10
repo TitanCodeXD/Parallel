fb056e2bfb86968e126d43fe42fdf1affd77f517
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-10 20:21:55 -0300

    Iniciando reducer de fotos

---
+++ frontend/src/slices/photoSlice.jsx
@@ -0,0 +1,27 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import photoService from "../services/photoService";
+
+const initialState = {
+    photos: [],
+    photo: {},
+    error: false,
+    success: false,
+    loading: false,
+    message: null,
+}
+
+// Functions
+
+
+export const photoSlice = createSlice({
+    name: "photo",
+    initialState,
+    reducers: {
+        resetMessage: (state) => {
+            state.message = null;
+        },
+    },
+});
+
+export const {resetMessage} = photoSlice.actions;
+export default photoSlice.reducer;
