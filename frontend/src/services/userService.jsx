895411d3f3c8ee03f5d27723327a41d81a4d5ae2
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-09 23:22:01 -0300

    Service de atualização de usuário

--- frontend/src/services/userService.jsx
+++ frontend/src/services/userService.jsx
@@ -18,8 +18,26 @@ const profile = async(data, token) => {
     }
 };
 
+// Update user details
+const updateProfile = async(data, token) => {
+
+    const config = requestConfig("PUT", data, token, true)
+
+    try {
+        
+        const res = await fetch(api + "/users/", config)
+                    .then((res) => res.json())
+                    .catch((err) => err);
+        return res;
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
 const userService = {
     profile,
+    updateProfile,
 };
 
 export default userService;
