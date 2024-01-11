79ba59736d840396d4e744da4c5fef67a88a8141
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-11 15:02:44 -0300

    Slice de inserção de foto

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -10,7 +10,24 @@ const initialState = {
     message: null,
 }
 
-// Functions
+// Publish user photo
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async(photo, thunkAPI) => {
+        
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.publishPhoto(photo, token)
+
+        // Check for errors
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
+);
+
 
 
 export const photoSlice = createSlice({
@@ -21,6 +38,28 @@ export const photoSlice = createSlice({
             state.message = null;
         },
     },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(publishPhoto.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(publishPhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photo = action.payload;
+                state.photos.unshift(state.photo);
+                state.message = "Foto publicada com sucesso!";
+            })
+            .addCase(publishPhoto.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
+            });
+    },
+
 });
 
 export const {resetMessage} = photoSlice.actions;
