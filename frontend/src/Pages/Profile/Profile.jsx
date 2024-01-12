7db8d7d76ec87e68a4a5c23ecf930446df284421
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-12 02:18:20 -0300

    Exibindo fotos no perfil de usuário

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -17,7 +17,7 @@ import { useParams } from 'react-router-dom';
 
 // Redux
 import { getUserDetails } from '../../slices/userSlice';
-import { publishPhoto, resetMessage } from '../../slices/photoSlice';
+import { publishPhoto, resetMessage, getUserPhotos } from '../../slices/photoSlice';
 
 // CSS
 import './Profile.css';
@@ -47,6 +47,7 @@ const Profile = () => {
   // Load user data
   useEffect(() => {
     dispatch(getUserDetails(id));
+    dispatch(getUserPhotos(id));
   }, [dispatch, id])
 
   const handleFile = (e) => {
@@ -118,6 +119,27 @@ dispatch(publishPhoto(formData))
           {messagePhoto && <Message msg = {messagePhoto} type = "success" />}
           </>
         )}
+        <div className = 'user-photos'>
+          <h2>Fotos publicadas:</h2>
+          <div className = 'photos-container'>
+          {photos && photos.map((photo) => (
+            <div className="photo" key = {photo._id}>
+              {photo.image && (<img 
+              src = {`${uploads}/photos/${photo.image}`} 
+              alt = {photo.title}
+              />
+              )}
+              {id === userAuth._id ? (
+                <p>actions</p>
+              ) : (<Link className = "btn" to = {`/photos/${photo._id}`}>
+                Ver
+                </Link>
+                )}
+            </div>
+          ))}
+          {photos.length === 0 && <p>Ainda não há fotos publicadas.</p>}
+          </div>
+        </div>
     </div>
   )
 }
