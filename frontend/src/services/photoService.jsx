376f3c05d55cc845bdf1b36246ceb7a7515bf65b
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-12 03:41:21 -0300

    Service de exclusão de fotos

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -33,7 +33,24 @@ const getUserPhotos = async(id, token) => {
     } catch (error) {
         console.log(error)
     }
-}
+};
+
+// Delete a photo
+const deletePhoto = async(id, token) => {
+
+    const config = requestConfig("DELETE", null, token)
+
+    try {
+
+        const res = await fetch(api + "/photos/" + id, config)
+                    .then((res) => res.json())
+                    .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error)
+    }
+};
 
 
 
@@ -41,6 +58,7 @@ const getUserPhotos = async(id, token) => {
 const photoService = {
     publishPhoto,
     getUserPhotos,
+    deletePhoto,
 };
 
 export default photoService;
