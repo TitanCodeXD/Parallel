61b914010ab7a289fe9d91da8b4a95c5bd52149f
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-12 03:49:30 -0300

    Slice de remoção de foto

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -39,9 +39,25 @@ export const getUserPhotos = createAsyncThunk(
 
         return data;
     }
+); 
+
+// Delete a photo
+export const deletePhoto = createAsyncThunk(
+    "photo/delete",
+    async(id, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.deletePhoto(id, token)
+
+        // Check for errors
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
 );
-
-
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -79,7 +95,27 @@ export const photoSlice = createSlice({
                 state.success = true;
                 state.error = null;
                 state.photos = action.payload;
-              });
+              })
+              .addCase(deletePhoto.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(deletePhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+
+                state.photos = state.photos.filter((photo) => {
+                    return photo._id !== action.payload.id
+                })
+
+                state.message = action.payload.message;
+              })
+            .addCase(deletePhoto.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+                state.photo = {};
+            });
     },
 
 });
