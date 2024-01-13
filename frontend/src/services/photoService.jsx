d3543a353ba0eb55fbe1c8a1d67365d82ccf30ad
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-13 01:12:37 -0300

    Service da foto pelo id

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -67,7 +67,23 @@ const updatePhoto = async(data, id, token) => {
     } catch (error) {
         console.log(error)
     }
-}
+};
+
+// Get a photo by id
+const getPhoto = async(id) => {
+
+    const config = requestConfig("GET")
+
+    try {
+        const res = await fetch(api + "/photos/" + id, config)
+                    .then((res) => res.json())
+                    .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error)
+    }
+};
 
 
 
@@ -77,6 +93,7 @@ const photoService = {
     getUserPhotos,
     deletePhoto,
     updatePhoto,
+    getPhoto,
 };
 
 export default photoService;
