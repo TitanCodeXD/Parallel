55cbfd8cbbdbfc151e80a4b6b489e8e4045ab337
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-14 01:53:07 -0300

    Finalizando Home

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   const dispatch = useDispatch();
 
-  const resetMessage = useResetComponentMessage();
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const {user} = useSelector((state) => state.auth);
   const {photos, loading} = useSelector((state) => state.photo);
@@ -50,7 +50,20 @@ const Home = () => {
   return (
 
 
-    <div>Home</div>
+    <div id = "home">
+      {photos && photos.map((photo) => (
+        <div key = {photo._id}>
+          <PhotoItem photo = {photo}/>
+          <LikeContainer photo = {photo} user = {user} handleLike = {handleLike}/>
+          <Link className = 'btn' to = {`/photos/${photo._id}`}>Ver mais</Link>
+        </div>
+      ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas, <Link to = {`/users/${user._Id}`}>Clique Aqui</Link>
+        </h2>
+      )}
+    </div>
   )
 }
 
