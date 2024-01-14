f5e203c868f05cdde0699527e8671321462eaefd
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-13 23:32:21 -0300

    Slice de comentário

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -110,6 +110,27 @@ export const like = createAsyncThunk(
     }
 );
 
+// Add comment to a photo
+export const comment = createAsyncThunk(
+    "photo/comment",
+    async(photoData, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.comment(
+            {comment: photoData.comment},
+            photoData.id, 
+            token);
+
+    // Check for errors
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+
+        return data;
+    }
+);
+
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -220,6 +241,19 @@ export const photoSlice = createSlice({
                 state.message = action.payload.message;
               })
             .addCase(like.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            })
+            .addCase(comment.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+
+                state.photo.comments.push(action.payload.comment);
+
+                state.message = action.payload.message;
+              })
+            .addCase(comment.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload
             });
