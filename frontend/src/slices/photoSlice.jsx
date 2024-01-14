a5c0173c73ea9fe5e3cd5c9d9892dc1eb2fcbc82
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-13 21:46:57 -0300

    Slice de likes

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -91,6 +91,26 @@ export const getPhoto = createAsyncThunk("photo/getphoto", async(id, thunkAPI) =
         return data;
     });
 
+
+// Like a photo
+export const like = createAsyncThunk(
+    "photo/like", 
+    async(id, thunkAPI) => {
+
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.like(id, token);
+
+    // Check for errors
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+
+    return data;
+    }
+);
+
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -180,7 +200,29 @@ export const photoSlice = createSlice({
                 state.success = true;
                 state.error = null;
                 state.photo = action.payload;
-              });
+              })
+              .addCase(like.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+
+                if(state.photo.likes){
+                    state.photo.likes.push(action.payload.userId)
+                }
+
+                state.photos.map((photo) => {
+                    if(photo._id === action.payload.photo.photoId){
+                        return photo.likes.push(action.payload.userId);
+                    }
+                    return photo;
+                });
+
+                state.message = action.payload.message;
+              })
+            .addCase(like.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload
+            });
     },
 
 });
