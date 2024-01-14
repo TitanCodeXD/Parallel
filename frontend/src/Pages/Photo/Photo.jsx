61eb8c82e4dda99301ada90a8ee3139597712d01
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-14 00:00:13 -0300

    Finalizando funcionalidade de comentário

--- frontend/src/Pages/Photo/Photo.jsx
+++ frontend/src/Pages/Photo/Photo.jsx
@@ -17,7 +17,7 @@ import { useParams } from 'react-router-dom';
 import { useResetComponentMessage } from '../../hooks/useResetComponentMessage';
 
 // Redux
-import { getPhoto, like } from '../../slices/photoSlice';
+import { getPhoto, like, comment } from '../../slices/photoSlice';
 
 // CSS
 import './Photo.css';
@@ -34,6 +34,7 @@ const Photo = () => {
     const {photo, loading, error, message} = useSelector((state) => state.photo);
 
     // Comentários
+    const [commentText, setCommentText] = useState("");
 
     // Load photo data
     useEffect(() => {
@@ -41,8 +42,25 @@ const Photo = () => {
     }, [dispatch, id]);
 
 
+    // Insert a like
     const handleLike = () => {
       dispatch(like(photo._id));
+
+      resetMessagee();
+    };
+
+    // Insert a comment
+    const handleComment = (e) => {
+      e.preventDefault();
+
+      const commentData = {
+        comment: commentText,
+        id: photo._id
+      }
+
+      dispatch(comment(commentData));
+
+      setCommentText("");
 
       resetMessagee();
     };
@@ -59,6 +77,31 @@ const Photo = () => {
         <div className = 'message-container'>
         {error && <Message msg = {error} type = "error"/>}
         {message && <Message msg = {message} type = "success"/>}
+        </div>
+        <div className = 'comments'>
+            {photo.comments && (
+              <>
+                <h3>Comentários: ({photo.comments.length})</h3>
+            <form onSubmit = {handleComment}>
+              <input type = "text" placeholder = 'Insira um comentário' onChange = {(e) => setCommentText(e.target.value)} value = {commentText || ""}/>
+              <input type = "submit" value = "Enviar" />
+            </form>
+            {photo.comments.length === 0 && <p>Não há comentários...</p>}
+            {photo.comments.map((comment) => (
+                <div className = 'comment' key = {comment.comment}> 
+                    <div className="author">
+                      {comment.userImage && (
+                         <img src = {`${uploads}/users/${comment.userImage}`} alt = {comment.userName}/>
+                      )}
+                      <Link to = {`/users/${comment.userId}`}>
+                        <p>{comment.userName}</p>
+                      </Link>
+                    </div>
+                    <p>{comment.comment}</p>
+                </div>
+            ))}
+              </>
+            )}
         </div>
     </div>
   )
--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -113,13 +113,13 @@ export const like = createAsyncThunk(
 // Add comment to a photo
 export const comment = createAsyncThunk(
     "photo/comment",
-    async(photoData, thunkAPI) => {
+    async(commentData, thunkAPI) => {
 
         const token = thunkAPI.getState().auth.user.token;
 
         const data = await photoService.comment(
-            {comment: photoData.comment},
-            photoData.id, 
+            {comment: commentData.comment},
+            commentData.id, 
             token);
 
     // Check for errors
