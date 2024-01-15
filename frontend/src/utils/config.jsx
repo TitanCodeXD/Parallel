0c43a596a28ca5b778a0a5b0293d80194ece1c11
Wesley Santos <wesleysantos32892653@outlook.com>
2024-01-15 15:17:33 -0300

    Ajustes frontend

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api"
-export const uploads = "http://localhost:5000/uploads"
+export const api = import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api"
+export const uploads = import.meta.env.REACT_APP_BACKEND_URL || "http://localhost:5000/uploads"
 
 export const requestConfig = (method, data, token = null, image = null) => {
 
